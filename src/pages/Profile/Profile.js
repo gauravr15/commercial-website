a0382b0a853b7daec3332d83c617938bba7a77ee
gauravr15 <gauravbhasker8@gmail.com>
2024-10-02 00:05:18 +0530

    change in Profile.js file

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
-const Home = () => {
+const Profile = () => {
   return (
     <>
       <Header />
